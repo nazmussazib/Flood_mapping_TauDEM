f061f5c73efe3893341a3a6374bf25aba7b5a5cd
Nazmus Sazib <nazmus.s.sazib@aggiemail.usu.edu>
2016-03-18 21:06:14 -0600

    change input instruction

--- Preprocess_HAS_Run.py
+++ Preprocess_HAS_Run.py
@@ -6,8 +6,9 @@ from Preprocessing_HAS import *
 
 ## change all inputs
 
-input_dir_name=r'E:\USU_Research_work\NFIE_Flood_Mapping\Onion'
-watershed_shapefile='onionwatershed_diss.shp'
+input_dir_name=r'E:\USU_Research_work\NFIE_Flood_Mapping\Onion' ##  where "Main_Watershed"  is located
+## please check whether all input files are located inside the "Main_Watershed" directory
+watershed_shapefile='onionwatershed_diss.shp' ##
 watershed_raster='Onionw.tif'
 droptostream_raster="Oniondd.tif"
 dinfslope_raster="Onionslp.tif"
