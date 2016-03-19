6bbcbf27ead4317847c0ef05e1e986460cf213e9
Nazmus Sazib <nazmus.s.sazib@aggiemail.usu.edu>
2016-03-18 20:37:01 -0600

    remove dependency on Fiona and GeoPandas

--- Preprocessing_HAS.py
+++ Preprocessing_HAS.py
@@ -1,88 +1,95 @@
 __author__ = 'shams'
-__author__ = 'shams'
 
-import shutil
+
 import os
-import fiona
-from osgeo import gdal
-import glob
+from osgeo import gdal, ogr
 import subprocess
 
 
 def PreProcess_Height_Above_Stream(input_dir_name,watershed_shapefile,watershed_raster,droptostream_raster,dinfslope_raster):
     input_dir1=input_dir_name+"\\Main_Watershed"
     infile = input_dir1+"\\"+watershed_shapefile
-    output_dir1=input_dir_name+"\\Subwatershed"
+
     if not os.path.exists(input_dir1):
        os.makedirs(input_dir1)
-    if not os.path.exists(output_dir1):
-       os.makedirs(output_dir1)
+
     output_dir2=input_dir_name+"\\Subwatershed_ALL"
     if not os.path.exists(output_dir2):
        os.makedirs(output_dir2)
 
-
-    with fiona.open(infile) as source:
-        meta = source.meta
-        for f in source:
-            dest = output_dir1
-            outfile = os.path.join(dest, "subwatershed_%s.shp" % f['properties']['GRIDCODE'])
-            with fiona.open(outfile, 'w', **meta) as sink:
-                sink.write(f)
-
-
-# # make directory for each of the subwatershed and then extract and keep subwatershed, complimentary watershed shapefile
-    with fiona.open(infile) as source:
-       source_dir=output_dir1
-       os.chdir(source_dir)
-       for f in source:
-        dest = output_dir2+"\\Subwatershed"+str(f['properties']['GRIDCODE'])
-        os.mkdir(dest)
-        sub=os.path.join(source_dir,"subwatershed_"+str(f['properties']['GRIDCODE'])+".*")
-        files_sub=glob.glob(sub)
-        for file1 in files_sub:
-            if os.path.isfile(file1):
-                shutil.copy2(file1, dest)
+    main_dir=input_dir1;subwshed_file=watershed_raster;dts_file=droptostream_raster;dinfslope_file=dinfslope_raster
+    # Get the input Layer
+    inShapefile =infile
+    inDriver = ogr.GetDriverByName("ESRI Shapefile")
+    inDataSource = inDriver.Open(inShapefile, 0)
+    inLayer = inDataSource.GetLayer()
+    spatialRef = inLayer.GetSpatialRef()
 
 
+# loop through input feature
+    for i in range(0, inLayer.GetFeatureCount()):
+        ## create directory
+      dest = output_dir2+"\\Subwatershed"+str(i)
+      os.mkdir(dest)
+# Get the input Feature
+      inFeature = inLayer.GetFeature(i)
+# Create output Feature
+      os.chdir(dest)
+       # Create the output Layer
+      outShapefile =dest+"\\"+"subwatershed_"+str(i)+".shp"
+      outDriver = ogr.GetDriverByName("ESRI Shapefile")
+
+# Remove output shapefile if it already exists
+      if os.path.exists(outShapefile):
+          outDriver.DeleteDataSource(outShapefile)
+
+# Create the output shapefile
+      outDataSource = outDriver.CreateDataSource(outShapefile)
+      outLayer = outDataSource.CreateLayer("subwatershed_"+str(i),  spatialRef ,geom_type=ogr.wkbPolygon)
+
+# Add input Layer Fields to the output Layer
+      inLayerDefn = inLayer.GetLayerDefn()
+      for k in range(0, inLayerDefn.GetFieldCount()):
+          fieldDefn = inLayerDefn.GetFieldDefn(k)
+          outLayer.CreateField(fieldDefn)
+    # Get the output Layer's Feature Definition
+      outLayerDefn = outLayer.GetLayerDefn()
+      outFeature = ogr.Feature(outLayerDefn)
+# Add field values from input Layer
+      for m in range(0, outLayerDefn.GetFieldCount()):
+             outFeature.SetField(outLayerDefn.GetFieldDefn(m).GetNameRef(), inFeature.GetField(m))
+# get geometry
+      geom = inFeature.GetGeometryRef()
+
+      outFeature.SetGeometry(geom)
+# Add new feature to output Layer
+      outLayer.CreateFeature(outFeature)
+# close output file
+      outDataSource.Destroy()
+ ## open new output file for clipping
+      infile = outShapefile
+      inSource = inDriver.Open(infile, 0)
+      inlayer = inSource.GetLayer()
+      extent = inlayer.GetExtent()
+
+      subwaterdir=os.path.join(main_dir,subwshed_file);subwater_out_file=os.path.join(dest,'subwatershed_'+str(i)+"w.tif")
+      dtsdir=os.path.join(main_dir,dts_file);dts_out_file=os.path.join(dest,'subwatershed_'+str(i)+"dd.tif")
+      dinfslpdir=os.path.join(main_dir,dinfslope_file);dinfslp_out_file=os.path.join(dest,'subwatershed_'+str(i)+"slp.tif")
+      command_subw="gdalwarp -te " + str(extent[0]) + " " + str(extent[2]) + " " + str(extent[1]) + " " + str(extent[3]) + " -dstnodata -32768 -cutline " + outShapefile+ " -cl "+ "subwatershed_"+str(i)+ " " +  subwaterdir + " " + subwater_out_file
+      command_dd="gdalwarp -te " +str(extent[0]) + " " + str(extent[2]) + " " + str(extent[1]) + " " + str(extent[3])+ " -dstnodata -32768 -cutline " + outShapefile+ " -cl "+ "subwatershed_"+str(i) + " " + dtsdir + " " + dts_out_file
+      command_slp="gdalwarp -te " + str(extent[0]) + " " + str(extent[2]) + " " + str(extent[1]) + " " + str(extent[3])+ " -dstnodata -32768 -cutline " +outShapefile + " -cl "+ "subwatershed_"+str(i) + " " +  dinfslpdir + " " + dinfslp_out_file
+
+      subprocess.check_call(command_subw)
+      subprocess.check_call(command_dd)
+      subprocess.check_call(command_slp)
 
 
-#
-# # extract streamraster,flow direction, watershed grid for each of the subwateshed and store in the subwatershed directory
-    with fiona.open(infile) as source:
-       for f in source:
-            dir=output_dir2+"\\Subwatershed"+str(f['properties']['GRIDCODE'])
-            os.chdir(dir)
-            inputfn = 'subwatershed_'+str(f['properties']['GRIDCODE'])+'.shp'                ##input file name
-
-            main_dir=input_dir1;subwshed_file=watershed_raster;dts_file=droptostream_raster;dinfslope_file=dinfslope_raster
-            subwaterdir=os.path.join(main_dir,subwshed_file);subwater_out_file=os.path.join(dir,'subwatershed_'+str(f['properties']['GRIDCODE'])+"w.tif")
-            dtsdir=os.path.join(main_dir,dts_file);dts_out_file=os.path.join(dir,'subwatershed_'+str(f['properties']['GRIDCODE'])+"dd.tif")
-            dinfslpdir=os.path.join(main_dir,dinfslope_file);dinfslp_out_file=os.path.join(dir,'subwatershed_'+str(f['properties']['GRIDCODE'])+"slp.tif")
-
-            input = fiona.open(inputfn, 'r')
-            xmin=str(input.bounds[0])
-            ymin=str(input.bounds[1])
-            xmax=str(input.bounds[2])
-            ymax=str(input.bounds[3])
-            layer_name=os.path.splitext(inputfn)[0]
-
-            command_subw="gdalwarp -te " + xmin + " " + ymin + " " + xmax + " " + ymax + " -dstnodata -32768 -cutline " + inputfn + " -cl "+ layer_name + " " +  subwaterdir + " " + subwater_out_file
-            command_dd="gdalwarp -te " + xmin + " " + ymin + " " + xmax + " " + ymax + " -dstnodata -32768 -cutline " + inputfn + " -cl "+ layer_name + " " + dtsdir + " " + dts_out_file
-            command_slp="gdalwarp -te " + xmin + " " + ymin + " " + xmax + " " + ymax + " -dstnodata -32768 -cutline " + inputfn + " -cl "+ layer_name + " " +  dinfslpdir + " " + dinfslp_out_file
-
-            subprocess.check_call(command_subw)
-            subprocess.check_call(command_dd)
-            subprocess.check_call(command_slp)
-
-            input.close()
+# Close DataSources
+    inDataSource.Destroy()
+    #outDataSource.Destroy()
 
 
-# #Remove subwatershed directory which we dont need
-    with fiona.open(infile) as source:
-      for f in source:
-        dest_dir=output_dir1
-        shutil.rmtree(dest_dir,ignore_errors=True)
+
 
 
 
